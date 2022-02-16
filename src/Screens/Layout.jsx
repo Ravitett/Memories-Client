9f284f18fa0690bd57970a08007203384b651e80
RotemEzra1 <rotem19961@gmail.com>
2022-02-16 17:23:43 +0200

    layout desing bulid 404

--- src/Screens/Layout.jsx
+++ src/Screens/Layout.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
 import Header from '../Components/Header'
@@ -8,15 +11,16 @@ import Footer from '../Components/Footer'
 
 const Layout = () => {
     return (
-        <>
+        <Box>
+            <ToastContainer rtl />
             <Header />
             <main>
-                <Container>
+                <Container sx={{my:3}}>
                     <Outlet />
                 </Container>
             </main>
             <Footer />
-        </>
+        </Box>
     )
 };
 
--- src/Screens/Page404.jsx
+++ src/Screens/Page404.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
-const Page404 = () => {
-    return (
-        <div>
-            <h1>Page 404</h1>
-        </div>
-    )
-}
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-export default Page404
+import { getAllMemoryForUser } from '../Api/memories'
+
+const User = () => {
+  let navigate = useNavigate();
+  const [data, setData] = useState([]);
+
+  useEffect(async () => {
+    let dataFromApi = await getAllMemoryForUser();
+    setData(dataFromApi);
+    console.log(dataFromApi);
+  }, [])
+
+  return (
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>תאריך</TableCell>
+            <TableCell>זיכרון</TableCell>
+            <TableCell>סטטוס</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <TableRow
+              key={row._id}
+              onClick={()=>{navigate(`/user/${row._id}`)}}
+              hover={true}
+              style={{ cursor: "pointer"}}
+            >
+               <TableCell>{row.date}</TableCell>
+               <TableCell>{row.title}</TableCell>
+               <TableCell>{row.status}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default User;
+
