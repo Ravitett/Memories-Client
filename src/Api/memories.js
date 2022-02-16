8a7a3f120bec16d77b55eb90d883dd4a91cbd388
Ravitett <ravitett@gmail.com>
2022-02-16 17:20:18 +0200

    memories  request workon

--- src/Api/memories.js
+++ src/Api/memories.js
@@ -3,6 +3,23 @@ import { httpService } from "./httpService";
 import axios from "axios";
 
 const middelPoint = "api/memories/";
+const USERID = localStorage.getItem("memories-userID") || '';
+
+const getAllMemories = async() => {
+    return httpService.get(`${middelPoint}`);
+}
+
+const getAllMemoryForUser = async() => {
+    return httpService.get(`${middelPoint}user/${USERID}`);
+}
+
+const getAllMemoryForApprove = async() => {
+    return httpService.get(`${middelPoint}manager/`);
+}
+
+const getMemoryForApprove = async(_id) => {
+    return httpService.get(`${middelPoint}manager/${_id}`);
+}
 
 const changeStatus = (_id,_newStatus) => {
     return httpService.put(`${middelPoint}status/${_id}`,{status:_newStatus});
@@ -11,22 +28,15 @@ const chatSendMsg = (_id,_from,_newMessage) => {
     return httpService.put(`${middelPoint}chat/${_id}`,{from: _from, message: _newMessage});
 }
 
-const getAllMemoryForApprove = async() => {
-    return httpService.get(`${middelPoint}manager/`);
-}
-
-const getAllMemories = async() => {
-    return httpService.get(`${middelPoint}`);
-}
-
-
 const getBadWords = async() => {
     return httpService.get(`${middelPoint}badwords/`)
 }
 
 export {
     getAllMemories,
+    getAllMemoryForUser,
     getAllMemoryForApprove,
+    getMemoryForApprove,
     changeStatus,
     getBadWords,
     chatSendMsg
